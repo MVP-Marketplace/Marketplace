fdaf359b6713217556634b63fca32e60eb6bf4e7
hugohp19 <hp19@hotmail.com>
2021-01-10 22:56:55 -0500

    Added controllers for builders (create, update, delete, get all and get one)

--- server/routes/secure/Builder.js
+++ server/routes/secure/Builder.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
-const { addProfile } = require('../../controllers/Builders');
+const {
+  addProfile,
+  getSpecificBuilderInfo,
+  getAllBuilderInfo,
+  updateBuilder,
+  deleteOneBuilder,
+} = require('../../controllers/Builders');
 
-router.post('/', addProfile);
+router.post('/profile', addProfile);
+router.get('/profile/:id', getSpecificBuilderInfo);
+router.get('/profiles', getAllBuilderInfo);
+router.patch('/profile/:id', updateBuilder);
+router.delete('/profile/:id', deleteOneBuilder);
 
 module.exports = router;
