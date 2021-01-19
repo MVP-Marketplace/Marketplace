3cb4fc8b7fb3bbc74e6891ebdf406dba9dd1f43a
hugohp19 <hp19@hotmail.com>
2021-01-18 21:44:42 -0500

    deleted some comments

--- server/db/models/Level.js
+++ server/db/models/Level.js
@@ -8,27 +8,6 @@ const levelSchema = new mongoose.Schema({
   value: {
     type: String,
   },
-
-  // entry: {
-  //   owner_id: mongoose.Schema.Types.ObjectId,
-  //   ref: 'Builder',
-  //   value: 'Entry'
-  // },
-  // junior: {
-  //   owner_id: mongoose.Schema.Types.ObjectId,
-  //   ref: 'Builder',
-  //   value: 'Junior'
-  // },
-  // mid: {
-  //   owner_id: mongoose.Schema.Types.ObjectId,
-  //   ref: 'Builder',
-  //   value: 'Mid'
-  // },
-  // senior: {
-  //   owner_id: mongoose.Schema.Types.ObjectId,
-  //   ref: 'Builder',
-  //   value: 'Senior'
-  // }
 });
 
 const Level = mongoose.model('Level', levelSchema);
