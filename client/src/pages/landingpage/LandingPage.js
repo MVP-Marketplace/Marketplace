25b7e3ff600e7c77f96fac6bac1e8c70bf9d0c2f
ljgalaty <laurel.galaty@gmail.com>
2021-02-06 17:22:01 -0500

    fixed margin-top cutoff

--- client/src/pages/landingpage/LandingPage.js
+++ client/src/pages/landingpage/LandingPage.js
@@ -6,21 +6,19 @@ const LandingPage = () => {
   return (
     <div className='body'>
       <div className='blue-cover'>
-        <div className='content-wrapper'>
-          <div className='text-container'>
-            <p className='coming-soon'>Coming Soon</p>
-            <h1 className='header'>
-              Make It <span className='mvp'>MVP</span>
-            </h1>
-            <h3 className='description'>
-              A place where ideas and bootcamp grads unite to create meaningful
-              viable products
-            </h3>
-            <p className='more-information'>For more information:</p>
-            <p className='email'>mvp@makeitmvp.com</p>
-          </div>
-          <img src={graphic} className='graphic' />
+        <div className='text-container'>
+          <p className='coming-soon'>Coming Soon</p>
+          <h1 className='header'>
+            Make It <span className='mvp'>MVP</span>
+          </h1>
+          <h3 className='description'>
+            A place where ideas and bootcamp grads unite to create meaningful
+            viable products
+          </h3>
+          <p className='more-information'>For more information:</p>
+          <p className='email'>mvp@makeitmvp.com</p>
         </div>
+        <img src={graphic} className='graphic' />
       </div>
     </div>
   );
