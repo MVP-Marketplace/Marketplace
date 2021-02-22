53894c72961418eb5b991c8f27df4f838ea5fe85
tracilevine <televine@gmail.com>
2021-02-22 17:30:32 -0500

    footer complete;

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import './Footer.css';
+import FacebookLogo from './assets/facebook.png';
+import InstagramLogo from './assets/instagram.png';
+import Logo from './assets/logoHolder.png';
 import { Link } from 'react-router-dom';
+import { Card, Container } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div>
-      <div className='footer-wrapper'>
-        <div className='right-footer-wrapper'>
-          <div className='social-wrapper'>
-            <h4>Follow Us</h4>
-            <div className='social-images'>
-              <img src='../facebook.png' alt='facebook-logo' />
-              <img src='../instagram.png' alt='instagem-logo' />
-            </div>
-          </div>
-          <div className='logo-wrapper'>
-            <img
-              src='http://placehold.it/20x20undefined1'
-              className='logo'
-              alt='make-it-logo'
+    <>
+      <Container className='d-flex flex-row'>
+        <Card className='right-wrapper d-flex flex-row'>
+          <Card.Body className='d-flex flex-column social-wrapper'>
+            <Card.Text className='follow'> Follow Us</Card.Text>
+            <Card.Body className='d-flex flex-row'>
+              <Card.Img src={FacebookLogo} alt='facebook logo' id='fb-logo' />
+              <Card.Img
+                src={InstagramLogo}
+                alt='instagram logo'
+                id='insta-logo'
+              />
+            </Card.Body>
+          </Card.Body>
+          <Card.Body className='d-flex flex-row mvp-wrapper'>
+            <Card.Img
+              src='https://picsum.photos/200'
+              alt='Make It MVP Logo'
+              id='logo'
             />
-            <h4> Make It MVP</h4>
-          </div>
-          <div className='description-wrapper'>
-            <p>
+            <Card.Text className='mvp'>Make It MVP</Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Text className='description-wrapper'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt.
-            </p>
-          </div>
-        </div>
-        <div className='left-wrapper'>
-          <div>
-            <Link>Contact Us</Link>
-          </div>
-          <div>
-            <Link>Partners</Link>
-          </div>
-          <div>
-            <Link>FAQs</Link>
-          </div>
-        </div>
-      </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className='left-wrapper d-flex flex-row'>
+          <Link className='link'>Contact Us</Link>
+          <Link className='link'>Partners</Link>
+          <Link className='link'>FAQs</Link>
+        </Card>
+      </Container>
       <h5>Copyright &#169; 2021 MakeItMVP. All rights reserved.</h5>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/journey/Journey.js
+++ client/src/pages/journey/Journey.js
@@ -2,37 +2,64 @@ import React from 'react';
 import './Journey.css';
 import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer.js';
-
+import { Card, Container } from 'react-bootstrap';
+import Polygon from './assets/Polygon.png';
+import Union from './assets/Union.png';
+import User from './assets/user.png';
+import Briefcase from './assets/briefcase.png';
 const Journey = () => {
   return (
-    <div>
-      <div className='wrapper'>
-        <div className='title-wrapper'>
-          <h1>
-            To begin,
-            <br></br> tell us which journey reflects you
+    <>
+      <div className='background'>
+        <Container className='wrapper'>
+          <h1 className='title-wrapper'>
+            To begin, <br /> tell us which journey reflects you
           </h1>
-        </div>
-        <div>
           <Link>
-            <div className='role-wrapper'>
-              <h2>Builder</h2>
-              <p>You are a developer or UX/UI product designer.</p>
-            </div>
+            <Card className='role-wrapper d-flex flex-row'>
+              <Card.Header className='card-image-wrapper'>
+                <Card.Img
+                  src={User}
+                  alt='polygon'
+                  id='user-icon'
+                  className='m-auto'
+                />
+              </Card.Header>
+              <Card.Body className='m-auto'>
+                <Card.Title className='card-title'> Builder</Card.Title>
+                <Card.Text className='card-text'>
+                  You are a coder ,UXUI product designer, a builder of magic on
+                  the screen
+                </Card.Text>
+              </Card.Body>
+              <Card.Img src={Union} alt='arrow' id='arrow' />
+            </Card>
           </Link>
           <Link>
-            <div className='role-wrapper'>
-              <h2>Founder</h2>
-              <p>
-                You maybe have a tech background or you have an idea that just
-                came to you in shower
-              </p>
-            </div>
+            <Card className='role-wrapper d-flex flex-row'>
+              <Card.Header className='card-image-wrapper'>
+                <Card.Img
+                  src={Briefcase}
+                  alt='polygon'
+                  id='user-icon'
+                  className='m-auto'
+                />
+              </Card.Header>
+
+              <Card.Body className='m-auto'>
+                <Card.Title className='card-title'>Founder</Card.Title>
+                <Card.Text className='card-text'>
+                  You maybe have a tech background or you have an idea that just
+                  came to you in shower
+                </Card.Text>
+              </Card.Body>
+              <Card.Img src={Union} alt='arrow' id='arrow' />
+            </Card>
           </Link>
-        </div>
+        </Container>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
